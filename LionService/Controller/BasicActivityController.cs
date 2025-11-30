4095518ddcfa02c81d8aaa1f3fd5659b5f72b4f6
Boris Truong <linhtch90@gmail.com>
2025-11-30 18:57:32 +0700

    Add Lion service

---
+++ LionService/Controller/BasicActivityController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LionService.Controller;
+
+[Route("api/lion/basic")]
+[ApiController]
+public class BasicActivityController: ControllerBase
+{
+    [HttpGet("info1")]
+    public IActionResult GetInfo1()
+    {
+        return Ok("This is Lion service - info1 route.");
+    }
+}
---
+++ LionService/Middleware/GlobalExceptionHandler.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Diagnostics;
+
+namespace LionService.Middleware;
+
+public static class GlobalExceptionHandler
+{
+    public static void UseCustomGlobalExceptionHandler(this IApplicationBuilder app)
+    {
+        app.UseExceptionHandler(errorApp =>
+        {
+            errorApp.Run(async context =>
+            {
+                var exceptionHandlerPathFeature = context.Features.Get<IExceptionHandlerPathFeature>();
+                var exception = exceptionHandlerPathFeature?.Error;
+
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+
+                await context.Response.WriteAsJsonAsync(new
+                {
+                    context.Response.StatusCode,
+                    Message = exception?.Message ?? "An unexpected error occurred."
+                });
+            });
+        });
+    }
+}
